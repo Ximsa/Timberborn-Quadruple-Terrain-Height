3e1f6308cb6aa61e31af56169f82abc57ffe2c3a
Ximsa <burrfelix@aol.de>
2021-09-21 22:06:49 +0200

    typo fix

--- Timberborn-Quadruple-Terrain-Height/QuadrupleTerrainHeightPlugin.cs
+++ Timberborn-Quadruple-Terrain-Height/QuadrupleTerrainHeightPlugin.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 using Timberborn.Core;
 namespace TimberbornQuadrupleTerrainHeight
 {
-    [BepInPlugin("org.bepinex.plugins.quadrupleterrainheight", "Quarduple Terrainheight", "1.0.0.0")]
+    [BepInPlugin("org.bepinex.plugins.quadrupleterrainheight", "Quadruple Terrainheight", "1.0.0.0")]
     public class QuadrupleTerrainHeightPlugin : BaseUnityPlugin
     {
         public void Awake()
