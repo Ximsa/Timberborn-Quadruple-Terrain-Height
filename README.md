b017cdef79971b62d6fd93574d5c377ed3f39a27
Ximsa <burrfelix@aol.de>
2021-09-21 21:34:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Timberborn Quadruple Terrain Height
+Quadruples the maximum terrain height to 64
---
+++ Timberborn-Quadruple-Terrain-Height/QuadrupleTerrainHeightPlugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using Timberborn.Core;
+namespace TimberbornQuadrupleTerrainHeight
+{
+    [BepInPlugin("org.bepinex.plugins.quadrupleterrainheight", "Quarduple Terrainheight", "1.0.0.0")]
+    public class QuadrupleTerrainHeightPlugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "org.bepinex.plugins.quadrupleterrainheight");
+        }
+    }
+
+    [HarmonyPatch(typeof(MapSize), "Initialize")]
+    public static class TerrainHeightPatch //Initialize
+    {
+        [HarmonyPrefix]
+        public static void setHeight(ref int ___MaxTerrainHeight) 
+        {
+            ___MaxTerrainHeight = 64;
+        }
+    }
+}
