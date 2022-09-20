2672dea206c712f1dd60d66384a51d1d15c3680a
Felix Burr <felix.burr@uni-ulm.de>
2022-09-20 14:11:06 +0200

    fix for Update 2

--- Timberborn-Quadruple-Terrain-Height/QuadrupleTerrainHeightPlugin.cs
+++ Timberborn-Quadruple-Terrain-Height/QuadrupleTerrainHeightPlugin.cs
@@ -5,15 +5,19 @@ using Timberborn.MapSystemUI;
 
 namespace TimberbornQuadrupleTerrainHeight
 {
-    [BepInPlugin("org.bepinex.plugins.quadrupleterrainheight", "Quadruple Terrainheight", "1.0.2")]
+    [BepInPlugin("org.bepinex.plugins.quadrupleterrainheight", "Quadruple Terrainheight", "1.1.0")]
     public class QuadrupleTerrainHeightPlugin : BaseUnityPlugin
     {
-	public void Awake()
-	{
-	    var height = typeof(MapSize).GetField("MaxTerrainHeight", BindingFlags.Static | BindingFlags.Public);
-	    var size = typeof(NewMapBox).GetField("MaxMapSize", BindingFlags.Static | BindingFlags.NonPublic);
-	    height.SetValue(null, (int)height.GetValue(null) * 4);
-	    size.SetValue(null, (int)size.GetValue(null) * 4);
-	}
+        public void Awake()
+        {
+            var factor = 4;
+            var height = typeof(MapSize).GetField("MaxMapEditorTerrainHeight", BindingFlags.Static | BindingFlags.Public);
+            var maxGameTerrainHeight = typeof(MapSize).GetField("MaxGameTerrainHeight", BindingFlags.Static | BindingFlags.Public);
+            var size = typeof(NewMapBox).GetField("MaxMapSize", BindingFlags.Static | BindingFlags.NonPublic);
+            var diff = (int)maxGameTerrainHeight.GetValue(null) - (int)height.GetValue(null);
+            height.SetValue(null, (int)height.GetValue(null) * factor);
+            maxGameTerrainHeight.SetValue(null, (int)height.GetValue(null) + diff);
+            size.SetValue(null, (int)size.GetValue(null) * factor);
+        }
     }
 }
