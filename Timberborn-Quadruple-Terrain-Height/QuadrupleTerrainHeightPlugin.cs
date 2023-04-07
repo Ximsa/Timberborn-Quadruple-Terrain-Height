8bde8ab4ff7a6e521ae710236b34d3a2ce2052c6
Felix Burr <felix.burr@uni-ulm.de>
2023-04-07 13:19:29 +0200

    added quadruple brush size

--- Timberborn-Quadruple-Terrain-Height/QuadrupleTerrainHeightPlugin.cs
+++ Timberborn-Quadruple-Terrain-Height/QuadrupleTerrainHeightPlugin.cs
@@ -2,22 +2,31 @@ using BepInEx;
 using System.Reflection;
 using Timberborn.Core;
 using Timberborn.MapSystemUI;
+using Timberborn.MapEditor;
+using System;
 
 namespace TimberbornQuadrupleTerrainHeight
 {
-    [BepInPlugin("org.bepinex.plugins.quadrupleterrainheight", "Quadruple Terrainheight", "1.1.0")]
+    [BepInPlugin("org.bepinex.plugins.quadrupleterrainheight", "Quadruple Terrainheight", "1.2.0")]
     public class QuadrupleTerrainHeightPlugin : BaseUnityPlugin
     {
         public void Awake()
         {
             var factor = 4;
-            var height = typeof(MapSize).GetField("MaxMapEditorTerrainHeight", BindingFlags.Static | BindingFlags.Public);
+            // terrain height
+            var maxEditorTerrainHeight = typeof(MapSize).GetField("MaxMapEditorTerrainHeight", BindingFlags.Static | BindingFlags.Public);
             var maxGameTerrainHeight = typeof(MapSize).GetField("MaxGameTerrainHeight", BindingFlags.Static | BindingFlags.Public);
-            var size = typeof(NewMapBox).GetField("MaxMapSize", BindingFlags.Static | BindingFlags.NonPublic);
-            var diff = (int)maxGameTerrainHeight.GetValue(null) - (int)height.GetValue(null);
-            height.SetValue(null, (int)height.GetValue(null) * factor);
-            maxGameTerrainHeight.SetValue(null, (int)height.GetValue(null) + diff);
-            size.SetValue(null, (int)size.GetValue(null) * factor);
+            var diff = (int)maxGameTerrainHeight.GetValue(null) - (int)maxEditorTerrainHeight.GetValue(null);
+            maxEditorTerrainHeight.SetValue(null, (int)maxEditorTerrainHeight.GetValue(null) * factor);
+            maxGameTerrainHeight.SetValue(null, (int)maxEditorTerrainHeight.GetValue(null) + diff);
+            // map size
+            var maxMapSize = typeof(NewMapBox).GetField("MaxMapSize", BindingFlags.Static | BindingFlags.NonPublic);
+            maxMapSize.SetValue(null, (int)maxMapSize.GetValue(null) * factor);
+            // brush size
+            var brushSize = Type
+                .GetType("Timberborn.MapEditor.BrushSizePanel, Timberborn.MapEditor, Version = 0.0.0.0, Culture = neutral, PublicKeyToken = null")
+                .GetField("MaxBrushSize", BindingFlags.Static | BindingFlags.NonPublic);
+            brushSize.SetValue(null, (int)brushSize.GetValue(null) * factor);
         }
     }
 }
